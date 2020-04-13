d6e70f8e6c1c21d151deb189c18286a983468181
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-13 18:55:16 +0200

    add golden-file test with expected results

---
+++ tests/test-golden.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# set current working directory to the directory of this script?
+
+TEST_DIR=$(dirname "$0")
+cd "${TEST_DIR}"
+
+test () {
+  echo ""
+  echo "___ Testing ${TEST_DIR}/$1.yaml ___"
+  npm start ${TEST_DIR}/$1.yaml > ./$1.stdout.log 2> ./$1.stderr.log
+
+  # Now, let's compare the output with the golden file (i.e. expected output)
+  git --no-pager diff $1.*.log
+  echo ""
+}
+
+# Test suite
+test sample-grading
+
+# Return a non-zero error code if any output file has changed
+exit $(git diff | wc -l)
