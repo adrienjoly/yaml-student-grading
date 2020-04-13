125abce78e5802732218128174b4059a3ff89c93
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-04-13 19:08:21 +0200

    fix(tests): run ts-node directly, to avoid noise in logs cf https://github.com/adrienjoly/yaml-student-grading/runs/583214636?check_suite_focus=true

--- tests/test-golden.sh
+++ tests/test-golden.sh
@@ -8,7 +8,7 @@ cd "${TEST_DIR}"
 test () {
   echo ""
   echo "___ Testing ${TEST_DIR}/$1.yaml ___"
-  npm start ${TEST_DIR}/$1.yaml > ./$1.stdout.log 2> ./$1.stderr.log
+  $(npm bin)/ts-node ../src/main.ts $1.yaml > ./$1.stdout.log 2> ./$1.stderr.log
 
   # Now, let's compare the output with the golden file (i.e. expected output)
   git --no-pager diff $1.*.log
